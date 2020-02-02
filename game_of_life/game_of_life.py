f27170f5dd27f6b34baf41f82128fba7621eada8
Tharun Tilakumara <47290042+DrCheeseFace@users.noreply.github.com>
2020-02-02 17:47:54 +0530

    ma final change
    im calling the project done!

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -10,33 +10,35 @@ def draw_grid(canvas_width,cells):
 
 def place_cell(event):
 	x = (event.x - (event.x % (canvas_width/cells)))/(canvas_width/cells)
-	y = (event.y - (event.y % (canvas_width/cells)))/(canvas_width/cells)
-		
-	canvas.create_rectangle(x*(canvas_width/cells),
-	y*(canvas_width/cells),
-	x*(canvas_width/cells)+canvas_width/cells,
-	y*(canvas_width/cells)+canvas_width/cells,
-	fill='black')
-	
-	board[int(x)][int(y)] = 1	
+	y = (event.y - (event.y % (canvas_width/cells)))/(canvas_width/cells)	
+	if board[int(x)][int(y)]!=1:		
+		canvas.create_rectangle(x*(canvas_width/cells),
+		y*(canvas_width/cells),
+		x*(canvas_width/cells)+canvas_width/cells,
+		y*(canvas_width/cells)+canvas_width/cells,
+		fill='black')
+		board[int(x)][int(y)] = 1	
 	print(x,y)
+
+
 
 def delete_cell(event):
 	x = (event.x - (event.x % (canvas_width/cells)))/(canvas_width/cells)
 	y = (event.y - (event.y % (canvas_width/cells)))/(canvas_width/cells)
+	if board[int(x)][int(y)]!=0:
+		canvas.create_rectangle(x*(canvas_width/cells),
+		y*(canvas_width/cells),
+		x*(canvas_width/cells)+canvas_width/cells,
+		y*(canvas_width/cells)+canvas_width/cells,
+		fill='white')
 
-	canvas.create_rectangle(x*(canvas_width/cells),
-	y*(canvas_width/cells),
-	x*(canvas_width/cells)+canvas_width/cells,
-	y*(canvas_width/cells)+canvas_width/cells,
-	fill='white')
-
-	board[int(x)][int(y)] = 0 	
+		board[int(x)][int(y)] = 0 	
 	print(x,y)
 
-def quit_drawing(event):
+def play(event):
 	global quit
-	quit=True
+	quit = True
+
 
 window = tkinter.Tk()
 window.title('Conways Game of Life')
@@ -47,7 +49,7 @@ canvas.pack()
 
 
 #number of cells per side
-cells = 100
+cells = 100 
 
 #loop to draw alive cells
 global board
@@ -56,25 +58,19 @@ canvas.bind("<B1-Motion>",place_cell) #accounts for both motion and clicks
 canvas.bind('<Button-1>',place_cell)
 canvas.bind("<B2-Motion>",delete_cell)  
 canvas.bind('<Button-2>',delete_cell)
-window.bind('<space>',quit_drawing)
+window.bind('<space>',play)
+
 draw_grid(canvas_width,cells)
 
 
 print(board)
-while True:
+while True:     #pause loop at the begginging 
 	window.update()
 	if quit==True:
-		del quit
 		break
 
-window.unbind('<Button-1>')
-window.unbind('<space>')
 
 
-
-
-
-#checking every cell and drawing alive or dead cell
 while True:
 	canvas.delete('all')	
 	draw_grid(canvas_width,cells)
@@ -89,8 +85,6 @@ while True:
 
 	window.update()
 	board=main.live_or_die(board,cells)
-
-
 
 
 window.mainloop()
--- game_of_life/main.py
+++ game_of_life/main.py
@@ -1,12 +1,13 @@
 #conway's game of life  
-#Tharun Tilakumara 2019/1/19 
+#Tharun Tilakumara 2020/1/19 
  
 import random
 
 #functions
 #checks and returns board for next generation
 def live_or_die(board,side_length):   #create temp_board return temp_board
-	temp_board=[[0 for i in range(side_length)]for j in range(side_length)]
+	temp_board = makeboard(side_length)
+	board = tuple(board)
 	
 	for i in range(side_length):
 		for j in range(side_length):
